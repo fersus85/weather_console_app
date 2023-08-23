46645665b5cd0eea0297b7f46b6c438eca51845c
fersus <deriabin_85@mail.ru>
2023-08-23 20:25:21 +0300

    Refactor coords.py, change whereami with geoloc.ru, add custom exceptions

--- weather/coords.py
+++ weather/coords.py
@@ -1,4 +1,9 @@
+import re
+
+from subprocess import Popen, PIPE
 from dataclasses import dataclass
+
+from exceptions import CantGetCoordinates
 
 
 @dataclass(slots=True, frozen=True)
@@ -7,5 +12,32 @@ class Coordinates:
     latitude: float
 
 
+adress = "https://www.geolocation.com/ru"
+
+
+def float_coor(value: str) -> float:
+    try:
+        return float(value)
+    except ValueError:
+        raise CantGetCoordinates
+
+
+def get_coors(resp):
+    latitude = re.findall(r"latitude.*\d{2}.\d{4}", resp)
+    longitude = re.findall(r"longitude.*\d{2}.\d{4}", resp)
+    return float_coor(longitude[0][12:]), float_coor(latitude[0][11:])
+
+
 def get_gps_coordinates() -> Coordinates:
-    return Coordinates(longitude=10, latitude=20)
+    process = Popen(['curl', adress], stdout=PIPE)
+    (output, err) = process.communicate()
+    exit_code = process.wait()
+    if err is not None or exit_code != 0:
+        raise CantGetCoordinates
+    coors = get_coors(output.decode().strip().lower())
+    return Coordinates(longitude=coors[0],
+                       latitude=coors[1])
+
+
+if __name__ == "__main__":
+    print(get_gps_coordinates())
